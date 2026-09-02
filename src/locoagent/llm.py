0ea229dcdfaef1dd87e069abb83dbb68f45b4df9
Skyltliu <skyltliu@gmail.com>
2026-09-02 15:40:14 -0400

    initial commit

---
+++ src/locoagent/llm.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+client = OpenAI(api_key=os.environ["LLM_API_KEY"], base_url=os.environ["LLM_API_BASE"],)
+
+model = os.environ["LLM_MODEL"]
+
+def ask_model(message:str) -> str:
+    response = client.responses.create(model=model, input=message,)
+    return response.output_text
